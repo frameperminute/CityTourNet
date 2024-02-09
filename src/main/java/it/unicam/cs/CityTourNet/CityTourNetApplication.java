67269d58a2f5c8a1c8b67b81691cf53b88759a47
frameperminute <flaviopompeo.menna@studenti.unicam.it>
2024-02-09 12:00:56 +0100

    Initial commit

---
+++ src/main/java/it/unicam/cs/CityTourNet/CityTourNetApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.CityTourNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityTourNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityTourNetApplication.class, args);
+	}
+
+}

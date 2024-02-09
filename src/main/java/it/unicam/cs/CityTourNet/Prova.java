25dd446850fb82b33589b781d724166f17bc1ab5
frameperminute <flaviopompeo.menna@studenti.unicam.it>
2024-02-09 12:25:22 +0100

    cambiamento classe Prova

---
+++ src/main/java/it/unicam/cs/CityTourNet/Prova.java
@@ -0,0 +1,20 @@
+package it.unicam.cs.CityTourNet;
+
+public class Prova {
+    private int index;
+
+    private String name;
+
+    Prova(int index, String name) {
+        this.index = index;
+        this.name = name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
